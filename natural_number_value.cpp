1084c905ab828fd462d34b251246fb4fd1cad109
Insu416 <jns0416@naver.com>
2018-01-14 22:05:56 +0900

    Create natural_number_value.cpp
    x + y + z = 100
    print all natural number values.

---
+++ natural_number_value.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int x=0, y=0, z=0;
+
+	x = 0;
+	while (x <= 100) {
+		y = 0;
+		while ((x+y) <= 100) {
+			z = 100 - x - y;
+
+			cout << x << " " << y << " " << z << endl;
+
+			y++;
+		}
+
+		x++;
+	}
+
+	return 0;
+}
